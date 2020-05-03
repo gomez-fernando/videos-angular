542c912fa0931ceeedf1088b834187d944dcf13b
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-05-03 14:39:56 +0200

    css updates

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-error',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
   public page_title: string;
+  private location: Location;
 
   constructor() {
     this.page_title = 'ERROR 404';
   }
 
   ngOnInit(): void {}
+
+  goBack() {
+    // window.history.back();
+    this.location.back();
+
+    console.log('goBack()...');
+  }
 }
